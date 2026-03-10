5d240c472c25426846f121d82a6dec1bd5da430b
ilmann911 <mohamadilmansyah09@gmail.com>
2026-03-10 10:09:38 +0700

    Rename class and package to ServisKomputer1

--- ServisKomputer1.java
+++ ServisKomputer1.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
-package com.mycompany.mavenproject2;
+package com.mycompany. ServisKomputer1;
 
 /**
  *
@@ -10,7 +10,7 @@ package com.mycompany.mavenproject2;
  */
 import java.util.Scanner;
 
-public class Mavenproject2 {
+public class  ServisKomputer1 {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
